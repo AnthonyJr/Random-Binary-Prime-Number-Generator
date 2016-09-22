834944475eec3b8a3ad0899e2a5e8811db4f81b9
AnthonyJr <tonyjunior5417@gmail.com>
2016-09-22 18:53:31 -0400

    Initial Commit

---
+++ Homework3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Homework3
+//
+//  Created by Anthony Snow on 9/22/16.
+//  Copyright © 2016 Anthony Snow. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
